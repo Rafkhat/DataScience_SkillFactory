f87c9fde920095ee5fc5f1f9a73f0bfc14843818
Rafkhat <rafkhatgabitov@gmail.com>
2023-02-11 00:51:49 +0300

    First commit

---
+++ game.py
@@ -0,0 +1,26 @@
+"""
+Game "Guess the number
+The computer guesses the number, and you guess
+"""
+
+import numpy as np
+number = np.random.randint(1, 101) # загадываем число: функция
+# random генерирует случайное число от 1 до 100.
+# Создаём переменную-счётчик для учёта количества попыток:
+
+count = 0
+
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100"))
+
+    if predict_number > number:
+        print("Число должно быть меньше!")
+
+    elif predict_number < number:
+        print("Число должно быть больше!")
+
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break #конец игры, выход из цикла
+    
